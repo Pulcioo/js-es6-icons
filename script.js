fee05339481f79693d4e652712f1793b44ed0143
Davide Pulci <davide.pulci@gmail.com>
2022-03-23 16:43:24 +0100

    visualizzate icone

--- script.js
+++ script.js
@@ -16,3 +16,139 @@ in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user).
 Quando l'utente seleziona un tipo dalla select, 
 visualizzare solamente le icone corrispondenti.
 */
+
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'fas',
+        color: 'orange'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+        color: 'green'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+        color: 'green'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+        color: 'green'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'fas',
+        color: 'green'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+        color: 'blue'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+        color: 'blue'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+        color: 'blue'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'fas',
+        color: 'blue'
+    }
+];
+
+// seleziono tramite query selector la classe .container-icon
+const containerIcon = document.querySelector('.container-icon');
+
+// ciclo array
+icons.forEach((icons, index, array) => {
+    creoBox(icons)
+});
+
+// funzione per creare box che contiene icon
+function creoBox(elemento) {
+    // creo elemento div
+    const box = document.createElement('div');
+    // lo inserisco nel documento come figlio di containerIcon
+    containerIcon.appendChild(box);
+    // assegno una classe
+    box.className = 'box-icon';
+    // modifico HTML
+    box.innerHTML = `<i class="${elemento.family} ${elemento.prefix}${elemento.name} fa-3x"></i>
+	<p class="text">${elemento.name}</p>`
+}
